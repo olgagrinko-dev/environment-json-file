16a5e48be65620c6a7a36a192a455605f98aac1a
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-04-24 15:01:41 +0300

    Node.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { getAll, createEnvironment, deleteEnvironment, updataEnvironment } = require('./service/servise.js');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/', (request, response) => {
+    try {
+        const data = getAll();
+        response.send(data);
+    } catch (error) {
+        response.send(error.message);
+    }
+});
+
+app.post('/', (request, response) => {
+    try {
+        const { label, category, priority } = request.body;
+        const data = createEnvironment(label, category, priority);
+        response.send(data);
+    } catch (error) {
+        response.send(error.message);
+    }
+});
+
+app.put('/:id', (request, response) => {
+    try {
+        const { id } = request.params;
+    } catch (error) {
+        response.send(error.message);
+    }
+});
+
+
+app.delete('/:id', (request, response) => {
+    try {
+        const { id } = request.params;
+        const data = deleteEnvironment(id);
+        response.send(data);
+    } catch (error) {
+        response.send(error.message);
+    }
+});
+
+app.listen(3000, () => console.log('server is running'));
+
---
+++ service/servise.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = './storage/storage.json';
+
+function getAll() {
+    const array = JSON.parse(fs.readFileSync(path));
+    if (!array.length) throw new Error('Массив пуст');
+    return array;
+};
+
+function createEnvironment(label, category, priority) {
+    const array = JSON.parse(fs.readFileSync(path));
+    array.push({
+        id: label.toLowerCase(),
+        label: label,
+        category: category,
+        priority: priority,
+    });
+    fs.writeFileSync(path, JSON.stringify(array));
+    return array;
+};
+
+function updataEnvironment(id, label, category, priority) {
+    const array = JSON.parse(fs.readFileSync(path));
+    const filtered = array.filter((elem) => elem.id != id);
+    if (filtered.length !== array.length) {
+        filtered.push({
+            id: id,
+            label: label,
+            category: category,
+            priority: priority,
+
+        });
+        return filtered;
+    } else {
+        return 'такого id нет';  
+    };
+    fs.writeFileSync(path, JSON.stringify(filtered));
+    return filtered;
+};
+
+function deleteEnvironment(id) {
+    const array = JSON.parse(fs.readFileSync(path));
+    const filtered = array.filter((elem) => elem.id != id);
+    fs.writeFileSync(path, JSON.stringify(filtered));
+    return filtered;
+};
+
+module.exports = { getAll, createEnvironment, deleteEnvironment, updataEnvironment };
